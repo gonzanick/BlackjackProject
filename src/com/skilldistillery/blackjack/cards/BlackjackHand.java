b75503b3419431f0ffe198893a91322a4064ab73
Nick G <gonzanick@gmail.com>
2022-04-08 15:19:12 -0600

    Added methods to classes

--- src/com/skilldistillery/blackjack/cards/BlackjackHand.java
+++ src/com/skilldistillery/blackjack/cards/BlackjackHand.java
@@ -1,5 +1,22 @@
 package com.skilldistillery.blackjack.cards;
 
 public class BlackjackHand {
+	
+	public BlackjackHand() {
+		
+	}
+	
+	public int getHandValue() {
+		return 0;
+	}
+	
+	public boolean isBlackjack() {
+		return true;
+	}
+	
+	public boolean isBust() {
+		return true;
+	}
+	
 
 }
--- src/com/skilldistillery/blackjack/cards/Hand.java
+++ src/com/skilldistillery/blackjack/cards/Hand.java
@@ -1,5 +1,32 @@
 package com.skilldistillery.blackjack.cards;
 
+import java.util.List;
+
 public abstract class Hand {
+	List<Cards> numOfCards;
+	
+	
+	public Hand() {
+		
+	}
+	
+	public void addCard(Cards card) {
+		
+	}
+	
+	public void clear() {
+		
+	}
+	
+	public int getHandValue() {
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		return "Hand [numOfCards=" + numOfCards + "]";
+	}
+	
+	
 
 }
